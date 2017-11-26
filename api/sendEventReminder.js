d98660c75dfbae1f9a1e2ae7052b2067049de82a
root <andries@filmer.nl>
2017-11-26 14:48:44 +0100

    sendEventReminder with mysql

--- api/sendEventReminder.js
+++ api/sendEventReminder.js
@@ -3,14 +3,13 @@ var moment = require('moment');
 
 var config = require('./config/config.js');
 var secret = require('./config/secret');
-var db = require('./config/mongo_database');
 
 // This script is only for Event reminders.
 //-----------------------------------------
 // I have a cron that runs each 5 minutes.
 // */5 * * * * export NODE_ENV=production /usr/bin/node /path/to/api_root/sendEventReminder.js
-var mStart = moment().add(1, 'days').toISOString();
-var mEnd   = moment().add(1, 'days').add(5, 'minutes').toISOString();
+var mStart = moment().add(1, 'days').format('YYYY-MM-DD HH:mm');
+var mEnd   = moment().add(1, 'days').add(5, 'minutes').format('YYYY-MM-DD HH:mm');
 
 // Debugging
 //console.log('Between -> gte: ' + mStart + ' And -> lt: ' + mEnd); 
@@ -20,17 +19,16 @@ getEvents(mStart, mEnd);
 
 function getEvents(mStart, mEnd) {
 
-  //var queryEvent = db.eventModel.find({"start": {"$gt": new Date(mStart), "$lte": new Date(mEnd)}});
-  var queryEvent = db.eventModel.find({"start": {"$gt": mStart, "$lte": mEnd}});
-  queryEvent.select("_id user_id title start end allDay description className created");
-  queryEvent.sort('start');
-  queryEvent.exec(function(err, results) {
-    if (err) {
-      console.log(err);
-    }
+  config.pool.getConnection(function(err, connection) {
 
-    // If we have a result dan send a reminder for each event.
-    if (typeof results === 'object') {
+    var sql = 'SELECT id as _id, user_id,title, start, end, allDay, tz, className \
+               FROM events WHERE start >= ? AND start <= ? LIMIT 10';
+
+    var query = connection.query(sql, [mStart, mEnd], function(err, results) {
+
+      connection.release();
+
+      if (err) throw err;
 
       results.forEach(function(event){
 
@@ -42,34 +40,34 @@ function getEvents(mStart, mEnd) {
         getUser(event);
 
       });
-    }
+    });
   });
-
-
 };
 
 // Get user email address
 function getUser(event) {
 
-  var queryUser = db.userModel.find({_id: { $in : [event.user_id]}});
-  queryUser.select("_id fullname email");
-  queryUser.exec(function(err, result) {
+  config.pool.getConnection(function(err, connection) {
 
-   if (err) {
-     console.log(err);
-   }
+    var sql = 'SELECT * FROM user WHERE id = ? AND active=1';
 
-   // Call for sending the reminder.
-   sendReminder(event, result);
+    var query = connection.query(sql, [event.user_id], function(err, result) {
 
+      connection.release();
+
+      if (err) throw err;
+
+      // Call for sending the reminder.
+       sendReminder(event, result);
+
+    });
   });
-
 }
 
 // Function for sending the email reminder.
 function sendReminder(event,user) {
 
-  var emailAddress = user[0].fullname + ' <' + user[0].email + '>' ; 
+  var emailAddress = user[0].name + ' <' + user[0].email + '>' ; 
   var transporter = nodemailer.createTransport({ 
     port: config.env().mail_port,
     ignoreTLS: true
