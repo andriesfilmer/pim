28bea56efd0e926b9b4328a203781dadfe7b91da
Andries Filmer <andries@filmer.nl>
2023-06-20 12:07:01 +0200

    working on contact addresses

--- app/javascript/components.js
+++ app/javascript/components.js
@@ -33,3 +33,50 @@ export function tooltip() {
   });
 
 }
+
+
+export function formChanged() {
+  let submitted = false
+  let userinput = false
+
+  let contactFormCheck = document.getElementById('contactForm')
+  contactFormCheck && contactFormCheck.addEventListener('submit', function(event) {
+    console.log("######## form submitted")
+    event.preventDefault()
+    return submitted = true
+  });
+
+  let userinputsCheck =  document.querySelector(".userinputs")
+  userinputsCheck && userinputsCheck.addEventListener("change", (event) => {
+    console.log("######## userinput changed")
+    $(".ib-cloud-upload").addClass("ib-cloud-upload-filled alert-color")
+    $(".ib-cloud-upload").removeClass("ib-cloud-upload")
+    return userinput = true
+  }, { once: true })
+
+  // If turbo off
+  window.onbeforeunload = function () {
+    if (userinput && !submitted) {
+      return false;
+    }
+  }
+
+  // If turbo on
+  addEventListener("turbo:before-visit", (event) => {
+    if (userinput && !submitted) {
+      let oke = confirm("Changes that you made may not be saved!")
+      if (oke) {
+      } else {
+        event.preventDefault()
+      }
+    }
+  }, { once: true })
+
+  return userinput
+}
+
+export function saveFormAlert() {
+  $(".ib-cloud-upload").addClass("ib-cloud-upload-filled alert-color")
+  $(".ib-cloud-upload").removeClass("ib-cloud-upload")
+}
+
--- app/javascript/controllers/contact_controller.js
+++ app/javascript/controllers/contact_controller.js
@@ -1,52 +1,56 @@
 // contact_controller.js
 import { Controller } from "@hotwired/stimulus";
 import { marked } from 'marked';
-import { tooltip } from 'components';
+import { tooltip, saveFormAlert } from 'components';
 
 let submitted = false
 let userinput = false
 
-function saveForm() {
-  $(".ib-cloud-upload").addClass("ib-cloud-upload-filled alert-color")
-  $(".ib-cloud-upload").removeClass("ib-cloud-upload")
+// Function for each type into a json object to store in one field in db.
+function prepareTypeValues(fieldId) {
+  console.log("######## fieldId: " + fieldId);
+  let typeValues = [];
+  let fieldIdArr = fieldId.split('_')
+  let type = fieldIdArr[1]
+  let divs = document.querySelectorAll('[id^="contact_' + type + '_"]');
+  [].forEach.call(divs, function(div) {
+    if ( 'contact_' + type + '_type' == div.id) {
+       typeValues.push('{"type"' + ':"' + div.value + '"');
+    } else {
+       typeValues.push('"value"' + ':"' + div.value + '"}');
+    }
+  });
+  document.getElementById('contact_' + type + 's').value = '[' + typeValues + ']';
 }
 
 export default class extends Controller {
 
   connect() {
-    console.log("######## connect contacts controller")
+    console.log("######## connect contact controller")
     //
     // Set cache control of current page to `no-cache`
     Turbo.cache.exemptPageFromCache()
     //
     //document.addEventListener("turbo:before-cache", function() {
-    //  console.log("######## before cache contacts controller");
+    //  console.log("######## before cache contact controller");
     //})
     //
     // Set cache control of current page to `no-preview`
     //Turbo.cache.exemptPageFromPreview()
 
+    // Show tooltips for this controller
     tooltip()
+
     $("#markdown").html(marked.parse($("#notes").text(),{ mangle: false, headerIds: false}))
 
-    $("form").on("input", function() {
-      saveForm()
-    });
+    // On each input change prepare the values  to store in db.
+    $(document).on('input', '[data-tojson]', function() {
+      prepareTypeValues(event.target.id)
+    })
 
-    // Prevent users from submitting a form by hitting Enter
-    $(document).on("keydown", ":input:not(textarea):not(:submit)", function(event) {
-      if(event.keyCode == 13) {
-        event.preventDefault()
-        return false
-      }
-    });
-
-    $( "#contactForm" ).on( "submit", function( event ) {
-      console.log("######## connect-> submit contacForm");
-      submitted = true;
-    });
-
-    $(".userinputs").change(function() {
+    //$(".userinputs").change(function() {
+    $(document).on('input', '.userinputs', function() {
+      saveFormAlert()
       return userinput = true;
     });
 
@@ -76,22 +80,21 @@ export default class extends Controller {
 
   setStarred() {
     if (document.getElementById('contact_starred').value == 'true') {
-      console.log("######## set starred false ");
       document.getElementById('contact_starred').value = 'false'
       $(".ib-star-filled").addClass("ib-star");
       $(".ib-star-filled").removeClass("ib-star-filled")
     } else {
-      console.log("######## set starred true ");
       document.getElementById('contact_starred').value = 'true'
       $(".ib-star").addClass("ib-star-filled");
       $(".ib-star").removeClass("ib-star");
     }
-    saveForm()
+    saveFormAlert()
+    return userinput = true;
   }
 
-  addPhone() {
-    console.log("######## addPhone");
-    const element = document.getElementById("phones")
+  addTypeValue(event) {
+    let addRow = event.target.getAttribute("data-value")
+    const element = document.getElementById(addRow + "s")
     const divRow = document.createElement("div")
     const divColumn1 = document.createElement("div")
     const divColumn2 = document.createElement("div")
@@ -103,23 +106,32 @@ export default class extends Controller {
     divColumn1.className = "small-5 medium-5"
     divColumn2.className = "small-6 medium-6 columns"
     divColumn3.className = "small-1 medium-1 columns"
-    divColumn3.setAttribute("data-action","click->contact#removePhone")
+    if (addRow === "phone") {
+      inputType.value = "Mobile"
+    } else if (addRow === "email") {
+      inputType.value = "Email"
+    }
     inputType.type = "text"
-    inputType.id = "contact_phone_type"
-    inputType.value = "Mobile"
-    inputType.placeholder = "Type..."
+    inputType.id = "contact_" + addRow + "_type"
+    inputType.setAttribute("data-tojson","")
     inputValue.type = "text"
-    inputValue.id = "contact_phone_value"
-    inputValue.placeholder = "+31......"
+    inputValue.id = "contact_" + addRow + "_value"
+    inputValue.setAttribute("data-tojson","")
+    inputValue.placeholder = "......"
     spanTrash.className = "ib-trash icon-medium"
+    spanTrash.setAttribute("data-action","click->contact#removeTypeValue")
+    spanTrash.setAttribute("data-value","contact_" + addRow)
 
     element.appendChild(divRow).appendChild(divColumn1).append(inputType)
     element.appendChild(divRow).appendChild(divColumn2).append(inputValue)
     element.appendChild(divRow).appendChild(divColumn3).append(spanTrash)
+    prepareTypeValues('contact_email_type')
   }
 
-  removePhone(e) {
-    e.target.parentNode.parentNode.remove();
+  removeTypeValue(event) {
+    event.target.parentNode.parentNode.remove();
+    prepareTypeValues(event.target.getAttribute("data-value"))
+    saveFormAlert()
   }
 
   showMarkdown() {
@@ -137,20 +149,7 @@ export default class extends Controller {
     $("#contact_notes").removeClass("display-none");
   }
 
-  prepareParams(event) {
-    event.preventDefault;
-    console.log("######## submit contact form");
-    let test = [];
-    let divs = document.querySelectorAll('[id^="contact_phone_"]');
-      [].forEach.call(divs, function(div) {
-      console.log("######## -> div.id: " + div.value);
-      if ( 'contact_phone_type' == div.id) {
-         test.push('{"type"' + ':"' + div.value + '"');
-      } else {
-         test.push('"value"' + ':"' + div.value + '"}');
-      }
-    });
-    document.getElementById('contact_phones').value = '[' + test + ']';
+  submitForm(event) {
     document.getElementById("contactForm").submit();
     return userinput = false
   }
