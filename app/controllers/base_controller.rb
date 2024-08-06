6230041627ac988c29c9c55582b49e1746c3be6a
Andries Filmer <andries@filmer.nl>
2024-08-06 21:41:29 +0200

    base index events order changed

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -2,8 +2,9 @@ class BaseController < ApplicationController
 
   def index
     @user = User.find current_user.id
-    @events = Event.where("start > ?", DateTime.now).where("start < ?", DateTime.now + 14.day)
-                   .where(user_id: current_user.id).order(start: :asc)
+    @events = Event.where("(start >= ? AND start <= ?) OR (end >= ? AND end <= ?)",
+     Date.today, Date.today + 14.day, Date.today, Date.today + 14.day)
+      .where(user_id: current_user.id).order(start: :asc)
   end
 
 end
