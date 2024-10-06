d4849a0de319519939856b6094a769706e8531a5
Andries Filmer <andries@filmer.nl>
2024-10-06 14:15:59 +0200

    Removed theme_sys switch in user profile

--- app/javascript/controllers/user_controller.js
+++ app/javascript/controllers/user_controller.js
@@ -5,14 +5,11 @@ export default class extends Controller {
   connect() {
     console.log("######## user controller connected");
 
-    //if (window.matchMedia('(prefers-color-scheme: light)').matches || window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    //  document.getElementById("theme_sys").checked = true;
-    if (localStorage.getItem("theme") === "dark-theme") {
-      document.getElementById("theme_dark").checked = true;
-    } else if (localStorage.getItem("theme") === "light-theme") {
+    // Set switch in profile to system, light or dark theme.
+    if (localStorage.getItem("theme") === "light-theme") {
       document.getElementById("theme_light").checked = true;
-    } else if (localStorage.getItem("theme") === "sys-theme") {
-      document.getElementById("theme_sys").checked = true;
+    } else if (localStorage.getItem("theme") === "dark-theme") {
+      document.getElementById("theme_dark").checked = true;
     } else {
       // Default light theme.
       document.getElementById("theme_light").checked = true;
@@ -22,19 +19,13 @@ export default class extends Controller {
   setTheme() {
     document.body.className = "";
     if (document.getElementById("theme_light").checked) {
-      document.body.classList.add("light-theme");
+      localStorage.removeItem("theme-sys");
       localStorage.setItem("theme", "light-theme");
+      document.body.classList.add("light-theme");
     } else if (document.getElementById("theme_dark").checked) {
-      document.body.classList.add("dark-theme");
+      localStorage.removeItem("theme-sys");
       localStorage.setItem("theme", "dark-theme");
-    } else if (document.getElementById("theme_sys").checked) {
-      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        document.body.classList.add("dark-theme");
-        localStorage.setItem("theme", "dark-theme");
-      } else {
-        document.body.classList.add("light-theme");
-        localStorage.setItem("theme", "light-theme");
-      }
+      document.body.classList.add("dark-theme");
     }
   }
 }
