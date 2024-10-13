970bac9da1d0703ef8fe9f9f2987ae92a63ed789
Andries <andries@gigabyte.home>
2024-10-13 12:51:40 +0200

    Anniversary + 1.year

--- Gemfile
+++ Gemfile
@@ -1,8 +1,7 @@
 source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-#ruby "3.2.3"
-ruby "3.0.2"
+ruby "3.2.3"
 
 gem "mysql2"
 gem "sqlite3", "~> 1.7"
--- app/mailers/base_mailer.rb
+++ app/mailers/base_mailer.rb
@@ -1,15 +1,15 @@
-class BaseMailer < ApplicationMailer
+# frozen_string_literal: true
 
+class BaseMailer < ApplicationMailer
   def birthdate_mail
     to = params[:to]
     @contact = params[:contact]
-    mail(to: to, subject: "Anniversary #{@contact.name} (#{@contact.age(@contact.birthdate)}) tomorrow")
+    mail(to: to, subject: "Anniversary #{@contact.name} (#{@contact.age(@contact.birthdate + 1.year)}) tomorrow")
   end
 
   def event_mail
     to = params[:to]
     @event = params[:event]
-    mail(to: to, subject: "Event #{@event.title} | #{@event.start.to_s[0,16]}")
+    mail(to: to, subject: "Event #{@event.title} | #{@event.start.to_s[0, 16]}")
   end
-
 end
