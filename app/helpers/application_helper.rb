3d6348831b50a198dc385bf7e493091c2ee5062c
Andries Filmer <andries@filmer.nl>
2026-01-16 17:17:27 +0100

    Diff fix in compare content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,8 @@ module ApplicationHelper
   def prepare_nice_data(first, second)
     first = first.sub(/^[^,]+,[^,]+,/, '') # Remove first two lines
     second = second.sub(/^[^,]+,[^,]+,/, '') # Remove first two lines
-    Diffy::Diff.new(second, first, :include_plus_and_minus_in_html => true).to_s(:html_simple).html_safe
+    diff_html = Diffy::Diff.new(second, first, :include_plus_and_minus_in_html => true).to_s(:html_simple)
+    diff_html.gsub('\n', '<br>').html_safe
   end
 
   def truthy? value
